760d45da2f7ef61f5cc32a01a4edd53a34928fac
dcarlantini <daiana.carlantini@mercadolibre.com>
2022-11-16 15:59:29 -0300

    Add dagger-hilt initial implementation

---
+++ app/src/main/java/com/example/githubchallenge/di/App.kt
@@ -0,0 +1,6 @@
+package com.example.githubchallenge.di
+
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : android.app.Application()
--- app/src/main/java/com/example/githubchallenge/view/ui/MainActivity.kt
+++ app/src/main/java/com/example/githubchallenge/view/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.githubchallenge.view.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.githubchallenge.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
